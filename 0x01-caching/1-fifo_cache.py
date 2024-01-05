749c7caa8beb4759c49c388cf5a005cad52fe448
Favourokerri <favourokerri767@gmail.com>
2024-01-05 09:08:47 +0000

    1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+FIFOCache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ class basicCache
+    """
+    def __init__(self):
+        """_summary_
+        """
+        super().__init__()
+        self.max_items = self.MAX_ITEMS
+
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+                key (_type_): _description_
+                item (_type_): _description_
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) > self.max_items:
+                removed_item = self.cache_data.popitem()
+                print(f"DISCARD: {removed_item}")
+                self.cache_data[key] = item
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ retrives item from cache """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
