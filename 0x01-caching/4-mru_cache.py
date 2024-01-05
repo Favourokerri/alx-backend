8b269f1e796c7d82ca0143edd74e5b01bf3a9416
Favourokerri <favourokerri767@gmail.com>
2024-01-05 14:08:39 +0000

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,67 +1,50 @@
 #!/usr/bin/env python3
-"""Create a class MRUCache that inherits
-from BaseCaching and is a caching system:
-
-You must use self.cache_data - dictionary
-from the parent class BaseCaching
-You can overload def __init__(self): but don’t
-forget to call the parent init: super().__init__()
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the
-item value for the key key.
-If key or item is None, this method should not do anything.
-If the number of items in self.cache_data is higher
-that BaseCaching.MAX_ITEMS:
-you must discard the most recently used item
-(MRU algorithm)
-you must print DISCARD: with the key discarded and
-following by a new line
-def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in
-self.cache_data, return None.
 """
-
-
-BaseCaching = __import__('base_caching').BaseCaching
+this module contains MRUCache tat inherits from BaseCaching and is
+ a caching sysytem
+"""
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """_summary_
-    """
-
+    """MRU Caching System"""
     def __init__(self):
-        """_summary_
+        """
+        instantiation of MRUCache class
         """
         super().__init__()
-        self.usedKeys = []
+        self.cache_data_order = []
 
     def put(self, key, item):
-        """_summary_
-
-        Args:
-                        key (_type_): _description_
-                        item (_type_): _description_
         """
-        if key is not None and item is not None:
+        add an item to a cache system
+        Args:
+           key(str): key of the item to add
+           item(str): the item to add
+        Returns: item if it is in the cache and None otherwise
+        """
+        if key and item:
             self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
+            if key not in self.cache_data_order:
+                self.cache_data_order.append(key)
             else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(-2)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+                self.cache_data_order.remove(key)
+                self.cache_data_order.append(key)
+            if len(self.cache_data_order) > BaseCaching.MAX_ITEMS:
+                length_c = len(self.cache_data_order)
+                popped = self.cache_data_order.pop(length_c - 2)
+                del self.cache_data[popped]
+                print("DISCARD: {}".format(str(popped)))
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
-
-        Args:
-                        key (_type_): _description_
         """
-        if key is not None and key in self.cache_data.keys():
-            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
-            return self.cache_data.get(key)
+        retrieve an item from cache
+        Args:
+            key(str): key of the item to return it
+        Returns: item if keu exists and None otherwise
+        """
+        if key in self.cache_data:
+            self.cache_data_order.remove(key)
+            self.cache_data_order.append(key)
+            return self.cache_data[key]
         return None
