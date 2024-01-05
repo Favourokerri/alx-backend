044ad41fe856f327d08f045cffd7938a4b990f07
Favourokerri <favourokerri767@gmail.com>
2024-01-05 08:22:44 +0000

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+BasicCache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ class basicCache
+    """
+    def put(self, key, item):
+        """ method to insert into cache """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ retrives item from cache """
+        if key is None or not key:
+            return None
+        else:
+            return self.cache_data.get(key)
